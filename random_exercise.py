6e96af7a4d20b553b774a25253708633b184aeb6
aryan8433 <109642913+aryan8433@users.noreply.github.com>
2026-01-20 09:35:05 -0800

    Add files via upload

---
+++ random_exercise.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import random
+
+chest_exercises = ["Pec Deck", "Bench Press", "Incline Bench Press"]
+tricep_exercises = ["Tricep Pressdown", "Overhead Extensions", "Tricep Kickback", "Dips"]
+back_exercises = ["Pull Ups", "T Bar Row", "Lat Pull Down"]
+bicep_exercises = ["Hammer Curls", "Preacher Curls", "Bayesian Cable curls", "Bicep Curls", "Inclined Dumbell Curl"]
+leg_exercises = ["Leg Pressdown", "Hamstring Curls", "Leg Extensions", "Calf Raises"]
+shoulder_exercises = ["Machine Shoulder Press", "Inclined Dumbell Y Raise", "Upright Row", "Cable Lateral Raise"]
+
+st.title("Workout Randomizer")
+
+day = st.selectbox("Choose workout day:", ["push", "pull", "legs"])
+
+if st.button("Generate Workout"):
+    if day == "push":
+        st.write("Chest:", random.sample(chest_exercises, 2))
+        st.write("Triceps:", random.sample(tricep_exercises, 2))
+    elif day == "pull":
+        st.write("Back:", random.sample(back_exercises, 2))
+        st.write("Biceps:", random.sample(bicep_exercises, 2))
+    elif day == "legs":
+        st.write("Shoulders:", random.sample(shoulder_exercises, 2))
+        st.write("Legs:", random.sample(leg_exercises, 2))
